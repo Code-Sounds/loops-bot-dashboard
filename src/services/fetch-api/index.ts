dc3c4f560eb0616bc662cb3fc759a7a6d6079cc6
edmilson-dk <droidvidaboa@gmail.com>
2021-10-14 11:57:15 -0300

    feat: created API services

---
+++ src/services/fetch-api/index.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+import { CONSTANTS } from "../../constants";
+
+const { USER_TOKEN_STORE_KEY, BASE_API } = CONSTANTS;
+
+const USER_TOKEN = localStorage.getItem(USER_TOKEN_STORE_KEY);
+const AuthStr = USER_TOKEN
+  ? { Authorization: "Bearer ".concat(USER_TOKEN) }
+  : undefined;
+
+export const API = axios.create({
+  baseURL: BASE_API,
+  headers: AuthStr,
+  validateStatus: (status) => {
+    if (status === 403) {
+      localStorage.clear();
+      window.location.href = "/login";
+      return false;
+    }
+
+    return true;
+  },
+});
