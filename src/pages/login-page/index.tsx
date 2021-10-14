9248c7981c718d9e5aa3cf13d0a32d26ba15e586
edmilson-dk <droidvidaboa@gmail.com>
2021-10-14 09:39:49 -0300

    feat: created initial login page structure

---
+++ src/pages/login-page/index.tsx
@@ -0,0 +1,11 @@
+export function LoginPage() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui,
+        voluptatum?
+      </p>
+    </div>
+  );
+}
