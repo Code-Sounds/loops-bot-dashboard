9098d1533e86dc994d6cb4616df82356be83fb94
edmilson-dk <droidvidaboa@gmail.com>
2021-10-16 17:24:41 -0300

    feat: created musics list component

---
+++ src/components/dashboard/musics/musics-list/index.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { API } from "../../../../services/fetch-api";
+import { MusicAPIData } from "../../../../types";
+import { MusicsListWrapper } from "./styles";
+
+type MusicsListResponse = {
+  musics: MusicAPIData[];
+};
+
+export function MusicsList() {
+  const [musics, setMusics] = useState<MusicAPIData[]>([]);
+
+  const {} = useQuery<MusicsListResponse>(
+    "musics",
+    async () => {
+      const { data } = await API.get<MusicsListResponse>("/musics/list");
+      return data;
+    },
+    {
+      onSuccess: (data) => setMusics(data.musics),
+      retry: 2,
+      refetchOnWindowFocus: true,
+    }
+  );
+
+  return (
+    <MusicsListWrapper>
+      <h1>Musics List</h1>
+    </MusicsListWrapper>
+  );
+}
---
+++ src/components/dashboard/musics/musics-list/styles.ts
@@ -0,0 +1,8 @@
+import { Stitches } from "../../../../styles/stitches";
+
+export const MusicsListWrapper = Stitches.styled("section", {
+  width: "100%",
+  padding: "$3",
+  backgroundColor: "$bgSecondary",
+  borderRadius: "$default",
+});
