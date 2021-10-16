3cb706774e6f77ce5395f27da3beeca130bde4ab
edmilson-dk <droidvidaboa@gmail.com>
2021-10-16 17:24:55 -0300

    feat: created music api data type

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,3 +5,14 @@ export type UserData = {
   createdAt: string;
   updatedAt: string;
 };
+
+export type MusicAPIData = {
+  id: string;
+  name: string;
+  artist: string;
+  url: string;
+  position: number;
+  createdAt: string;
+  updatedAt: string;
+  duration: number;
+};
