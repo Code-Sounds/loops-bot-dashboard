474a765a0c553692bc16745546a46464820ef548
edmilson-dk <droidvidaboa@gmail.com>
2021-10-24 12:16:38 -0300

    feat: added socket on get music playing events

--- src/pages/dashboard/musics/index.tsx
+++ src/pages/dashboard/musics/index.tsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react";
 import { MusicsList } from "../../../components/dashboard/musics/musics-list";
 import { Template } from "../../../components/dashboard/template";
+import { SOCKET_EVENTS } from "../../../constants";
+import { socket } from "../../../services/socket";
+import { MusicAPIData } from "../../../types";
 import { MusicsPageWrapper } from "./styles";
 
 export function MusicsPage() {
+  const [actualMusicPlaying, setActualMusicPlaying] =
+    useState<MusicAPIData | null>(null);
+
+  useEffect(() => {
+    socket.on(SOCKET_EVENTS.onMusicIsPlaying, (data: MusicAPIData) => {
+      setActualMusicPlaying(data || null);
+    });
+
+    socket.emit(SOCKET_EVENTS.getActualMusicPlaying, {});
+  }, []);
+
   return (
     <Template>
       <MusicsPageWrapper>
-        <MusicsList />
+        <MusicsList musicPlaying={actualMusicPlaying} />
       </MusicsPageWrapper>
     </Template>
   );
