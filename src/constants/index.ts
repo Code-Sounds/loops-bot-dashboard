3ccbde0951385a5cfd4440613acb57c4735c6b8a
edmilson-dk <droidvidaboa@gmail.com>
2021-11-01 09:06:58 -0300

    feat: added real api url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,8 @@
 export const CONSTANTS = {
   USER_TOKEN_STORE_KEY: "@loopsbot_usertoken",
   USER_STORE_KEY: "@loopsbot_user",
-  BASE_API: "http://localhost:8080/api",
-  BASE_SOCKET_URL: "http://localhost:8080",
+  BASE_API: "https://loops-bot-api.herokuapp.com/api",
+  BASE_SOCKET_URL: "https://loops-bot-api.herokuapp.com/",
 };
 
 export const SOCKET_EVENTS = {
