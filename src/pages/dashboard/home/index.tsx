6afbdd6b8f998b62df9f2f7aecb347aa1d526140
edmilson-dk <droidvidaboa@gmail.com>
2021-11-11 11:36:51 -0300

    feat: added get servers connected on realtime and musics count

--- src/pages/dashboard/home/index.tsx
+++ src/pages/dashboard/home/index.tsx
@@ -1,16 +1,52 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { FiClock, FiMusic, FiUsers } from "react-icons/fi";
+import { useQuery } from "react-query";
+
 import { InfosBox } from "../../../components/dashboard/infos-box";
 import { Template } from "../../../components/dashboard/template";
 import { SOCKET_EVENTS } from "../../../constants";
+import { API } from "../../../services/fetch-api";
 import { socket } from "../../../services/socket";
-import { ServerConnectedType } from "../../../types";
+import { MusicAPIData, ServerConnectedType } from "../../../types";
+import {
+  getStoredServersConnected,
+  storeServersConnected,
+} from "../../../utils/local-storage-utils";
 import { HomePageContent } from "./styles";
 
+type MusicsListResponse = {
+  musics: MusicAPIData[];
+};
+
 export function HomePage() {
+  const [serversConnected, setServersConnected] = useState<
+    ServerConnectedType[]
+  >(getStoredServersConnected());
+  const [musics, setMusics] = useState<MusicAPIData[]>([]);
+
+  useQuery<MusicsListResponse>(
+    "home-musics",
+    async () => {
+      const { data } = await API.get<MusicsListResponse>("/musics/list");
+      return data;
+    },
+    {
+      onSuccess: (data) => setMusics(data.musics),
+      retry: 2,
+      refetchOnWindowFocus: true,
+    }
+  );
+
   useEffect(() => {
     socket.on(SOCKET_EVENTS.serversConnected, (data: ServerConnectedType[]) => {
-      console.log(data);
+      setServersConnected(data);
+      storeServersConnected(data);
+    });
+    socket.on(SOCKET_EVENTS.addedNewMusic, (music: MusicAPIData) => {
+      setMusics((prevMusics) => [...prevMusics, music]);
+    });
+    socket.on(SOCKET_EVENTS.droppedMusic, (music: MusicAPIData) => {
+      setMusics((prevMusics) => prevMusics.filter((m) => m.id !== music.id));
     });
   }, []);
 
@@ -20,13 +56,13 @@ export function HomePage() {
         <InfosBox
           Icon={<FiUsers />}
           title="Servidores conectados"
-          value={20}
+          value={serversConnected.length}
           theme="orange"
         />
         <InfosBox
           Icon={<FiMusic />}
-          title="Músicas disponíveis"
-          value={3}
+          title="Quantidade de músicas"
+          value={musics.length}
           theme="green"
         />
         <InfosBox
