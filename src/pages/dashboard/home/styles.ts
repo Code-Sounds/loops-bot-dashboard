09eb017d9dfd2fceaffefb48c675c7463bf1ba21
edmilson-dk <droidvidaboa@gmail.com>
2021-11-11 11:13:13 -0300

    feat: created home content

---
+++ src/pages/dashboard/home/styles.ts
@@ -0,0 +1,10 @@
+import { Stitches } from "../../../styles/stitches";
+
+export const HomePageContent = Stitches.styled("main", {
+  width: "100%",
+  height: "100vh",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "$5",
+});
